9e6b0e495fc1c2e404e68766b1b56b83a335d2f2
srirams1003 <57574798+srirams1003@users.noreply.github.com>
2023-11-08 20:35:34 -0800

    Update firstScript.sh

--- firstScript.sh
+++ firstScript.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt install zsh nodejs npm vim tmux xsel
 sudo npm install n -g
 sudo n stable
+sudo npm install -g prettier
 sudo apt install curl wget git
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
