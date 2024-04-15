4e8cf0a480e0b97d6ce4b9b1aeeb336f24aaf67e
srirams1003 <57574798+srirams1003@users.noreply.github.com>
2024-04-15 11:14:56 -0700

    Update second_alt_kickstart.sh

--- second_alt_kickstart.sh
+++ second_alt_kickstart.sh
@@ -35,8 +35,9 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 # # ruby stuff
 # # this would also mean that you need to install ruby first. idk how to do that for ubuntu
 # # is chruby even a thing for ubuntu??
-# sudo gem install ruby-lsp
-# sudo gem install colorls
+sudo apt-get install ruby-full
+sudo gem install ruby-lsp
+sudo gem install colorls
 
 # I am also skipping installing java lsp for now since I don't ever use Java and it seems complicated and why would i lose some seconds in loading a plugin I never even use
 
