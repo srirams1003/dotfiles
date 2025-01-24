6ee862785a834e001fd1cca5d3827915b9d8ffeb
Sriram Suresh <sriram.suresh449@gmail.com>
2025-01-23 18:19:54 -0600

    added changes for macos env installaton

--- firstScript.sh
+++ firstScript.sh
@@ -1,11 +1,9 @@
 # first script
 
-sudo apt update -y
-sudo apt install zsh nodejs npm vim tmux xsel -y
+brew install nodejs npm vim tmux 
 sudo npm install n -g
 sudo n stable
 sudo npm install -g prettier markdown-it
-sudo apt install curl wget git -y
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
--- second_alt_kickstart.sh
+++ second_alt_kickstart.sh
@@ -5,13 +5,10 @@
 # do prefix + capital I inside a tmux session
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo add-apt-repository ppa:zhangsongcui3371/fastfetch -y
-sudo apt update -y
-sudo apt install make gcc ripgrep unzip neovim tldr picom playerctl feh i3 i3status i3blocks alacritty simplescreenrecorder psensor rofi lxappearance arandr copyq bat maim xclip xdotool fzf imagemagick python3-pip fastfetch -y
+brew install neovim fastfetch ripgrep unzip tldr copyq bat fzf 
+
 tldr -u
 
 # https://github.com/nvim-lua/kickstart.nvim --> refer to this repo
@@ -19,21 +16,8 @@ git clone https://github.com/srirams1003/lua-nvim-config.git "${XDG_CONFIG_HOME:
 
 git clone https://github.com/srirams1003/i3-dotfiles.git "${XDG_CONFIG_HOME:-$HOME/.config}"/i3
 
-sudo ln -sf ~/.config/i3/i3blocks.conf /etc/i3blocks.conf
-sudo ln -sf ~/.config/i3/i3status.conf /etc/i3status.conf # i use i3blocks instead of i3status but adding this too just in case
-ln -sf ~/.config/i3/.alacritty.toml ~/.alacritty.toml
-mkdir -p ~/.config/rofi
-ln -sf ~/.config/i3/config.rasi ~/.config/rofi/config.rasi
-
-# for rebinding esc to capslock and vice versa
-[ -e /usr/share/X11/xkb/symbols/pc ] && sudo mv /usr/share/X11/xkb/symbols/pc /usr/share/X11/xkb/symbols/pc.bak
-sudo ln -sf ~/.config/i3/usr-share-X11-xkb-symbols-pc /usr/share/X11/xkb/symbols/pc
 ln -sf ~/.config/i3/.p10k.zsh ~/.p10k.zsh
 ln -sf ~/.config/i3/.zshrc ~/.zshrc
-
-# so I can change the notification timeout for dunst to 3 secs from default of 10 secs
-[ -e /etc/xdg/dunst/dunstrc ] && sudo mv /etc/xdg/dunst/dunstrc /etc/xdg/dunst/dunstrc.bak
-sudo ln -sf ~/.config/i3/dunstrc /etc/xdg/dunst/dunstrc
 
 git config --global user.email "sriram.suresh449@gmail.com"
 git config --global user.name "Sriram Suresh"
@@ -43,11 +27,8 @@ git config --global credential.helper store  'cache --timeout=3000000'
 sudo npm i -g pyright
 sudo npm install -g diagnostic-languageserver
 sudo npm install -g typescript-language-server typescript
-sudo apt-get install clangd-12 -y
-sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
 sudo npm i -g vscode-langservers-extracted  # for html
 sudo npm i -g css-variables-language-server # for css
-sudo apt-get -y install golang-go gopls
 
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -55,18 +36,15 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 # # ruby stuff
 # # this would also mean that you need to install ruby first. idk how to do that for ubuntu
 # # is chruby even a thing for ubuntu??
-sudo apt-get -y install ruby-full
-sudo gem install ruby-lsp
-sudo gem install colorls
 
-# I am also skipping installing java lsp for now since I don't ever use Java and it seems complicated and why would i lose some seconds in loading a plugin I never even use
+brew install ruby-install chruby
+ruby-install --latest ruby
+gem install ruby-lsp
+gem install colorls
 
-# cp ~/dotfiles/Win10_WSL_zshrc.txt ~/.
-# mv ~/Win10_WSL_zshrc.txt ~/.zshrc
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# cp ~/dotfiles/tmux.conf ~/.tmux.conf # no longer using this as I want to add .tmux.conf to git too
 ln -sf ~/.config/i3/.tmux.conf ~/.tmux.conf
 
 source ~/.zshrc
@@ -75,8 +53,3 @@ chsh -s $(which zsh)
 
 cd ~/dotfiles
 
-sudo cpan JSON
-
-# lxappearance &
-
-flatpak install --noninteractive flathub hu.irl.cameractrls
